604b5e0809043e900046579f95a39870b431be01
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-05-23 15:42:37 -0400

    Track allocation rate + TPS

--- src/main/java/org/embeddedt/vintagefix/VintageFixClient.java
+++ src/main/java/org/embeddedt/vintagefix/VintageFixClient.java
@@ -6,24 +6,31 @@ import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.resources.FallbackResourceManager;
 import net.minecraft.client.resources.IResourcePack;
 import net.minecraft.client.resources.SimpleReloadableResourceManager;
+import net.minecraft.server.integrated.IntegratedServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.embeddedt.vintagefix.dynamicresources.CTMHelper;
 import org.embeddedt.vintagefix.dynamicresources.ResourcePackHelper;
 import org.embeddedt.vintagefix.impl.Deduplicator;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -85,5 +92,80 @@ public class VintageFixClient {
         }
         watch.stop();
         VintageFix.LOGGER.info("Texture search took {}", watch);
+    }
+
+    float lastIntegratedTickTime;
+    @SubscribeEvent
+    public void onTick(TickEvent.ServerTickEvent event) {
+        if(FMLCommonHandler.instance().getSide().isClient() && event.side.isServer() && event.phase == TickEvent.Phase.END) {
+            IntegratedServer srv = Minecraft.getMinecraft().getIntegratedServer();
+            if(srv != null) {
+                long currentTickTime = srv.tickTimeArray[srv.getTickCounter() % 100];
+                lastIntegratedTickTime = lastIntegratedTickTime * 0.8F + (float)currentTickTime / 1000000.0F * 0.2F;
+            } else
+                lastIntegratedTickTime = 0;
+        }
+    }
+
+    AllocationRateTracker tracker = new AllocationRateTracker();
+
+    @SubscribeEvent
+    public void onTick(TickEvent.ClientTickEvent event) {
+        if(event.phase == TickEvent.Phase.START)
+            tracker.tick();
+    }
+
+    // highest to minimize number of entries that need shuffling
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public void onRenderF3(RenderGameOverlayEvent.Text event) {
+        Minecraft minecraft = Minecraft.getMinecraft();
+        if(!minecraft.gameSettings.showDebugInfo)
+            return;
+        IntegratedServer srv = minecraft.getIntegratedServer();
+        if (srv != null) {
+            event.getLeft().add(2, String.format("Integrated server @ %.0f ms ticks", lastIntegratedTickTime));
+        }
+        event.getRight().add(2, String.format("Allocation rate: %03dMB /s", tracker.getAllocationRate()));
+    }
+
+    private static class AllocationRateTracker {
+        private static final List<GarbageCollectorMXBean> COLLECTORS = ManagementFactory.getGarbageCollectorMXBeans();
+        private static final long MINIMUM_TIME_AFTER_GC = 200;
+        long prevFreeMem = Runtime.getRuntime().freeMemory();
+        long timeBase = 0;
+        long lastGcCount = -1;
+        long allocationRate = 0;
+
+        void tick() {
+            long freeMem = Runtime.getRuntime().freeMemory();
+            if(gcCountChanged() || freeMem > prevFreeMem) {
+                prevFreeMem = freeMem;
+                timeBase = System.nanoTime();
+                return;
+            }
+            /* measure based on time */
+            long milliDelta = (System.nanoTime() - timeBase) / (1000 * 1000);
+            if(milliDelta <= MINIMUM_TIME_AFTER_GC)
+                return;
+            // bytes per millisecond
+            long byteChange = prevFreeMem - freeMem;
+            long allocationRateBytePerMilli = byteChange / milliDelta;
+            // convert to MB per second
+            allocationRate = allocationRateBytePerMilli * 1000L / (1000 * 1000);
+        }
+
+        boolean gcCountChanged() {
+            long c = 0;
+            for(GarbageCollectorMXBean collector : COLLECTORS) {
+                c += collector.getCollectionCount();
+            }
+            boolean isSame = lastGcCount == c;
+            lastGcCount = c;
+            return !isSame;
+        }
+
+        long getAllocationRate() {
+            return allocationRate;
+        }
     }
 }
