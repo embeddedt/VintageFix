76e0a8027fa39ae68d55fd4eb18760c56a311840
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-05-22 20:24:35 -0400

    Port dynamic structure manager from ModernFix

---
+++ src/main/java/org/embeddedt/vintagefix/mixin/dynstructures/MixinTemplateManager.java
@@ -0,0 +1,32 @@
+package org.embeddedt.vintagefix.mixin.dynstructures;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import net.minecraft.world.gen.structure.template.Template;
+import net.minecraft.world.gen.structure.template.TemplateManager;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+
+@Mixin(TemplateManager.class)
+public class MixinTemplateManager {
+    @Shadow
+    @Final
+    @Mutable
+    private Map<String, Template> templates;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void useDynamicMap(CallbackInfo ci) {
+        /* Structures needing to be reloaded is not a huge issue since we optimize loading them already */
+        Cache<String, Template> structureCache = CacheBuilder.newBuilder()
+            .softValues()
+            .build();
+        this.templates = structureCache.asMap();
+    }
+}
