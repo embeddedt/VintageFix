e0827d42424b61a45058008986abdf8d123a408c
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-05-22 22:15:57 -0400

    Try to handle ExtendedBlockStates

---
+++ src/main/java/org/embeddedt/vintagefix/mixin/fastmap/ExtendedStateImplementationMixin.java
@@ -0,0 +1,33 @@
+package org.embeddedt.vintagefix.mixin.fastmap;
+
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.block.Block;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import org.embeddedt.vintagefix.ducks.FastMapStateHolder;
+import org.embeddedt.vintagefix.fastmap.FastMap;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(targets = {"net/minecraftforge/common/property/ExtendedBlockState$ExtendedStateImplementation"})
+public abstract class ExtendedStateImplementationMixin extends BlockStateContainer.StateImplementation {
+    protected ExtendedStateImplementationMixin(Block blockIn, ImmutableMap<IProperty<?>, Comparable<?>> propertiesIn) {
+        super(blockIn, propertiesIn);
+    }
+
+    @Shadow
+    private IBlockState cleanState;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(CallbackInfo ci) {
+        IBlockState copyState = cleanState != null ? cleanState : getBlock().getBlockState().getBaseState();
+        FastMapStateHolder<IBlockState> otherHolder = (FastMapStateHolder<IBlockState>)copyState;
+        FastMapStateHolder<IBlockState> selfHolder = (FastMapStateHolder<IBlockState>)this;
+        selfHolder.setStateMap(otherHolder.getStateMap());
+        selfHolder.setStateIndex(otherHolder.getStateIndex());
+    }
+}
