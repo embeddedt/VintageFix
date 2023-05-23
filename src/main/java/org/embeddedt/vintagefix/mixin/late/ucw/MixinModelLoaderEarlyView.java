277f96f645fa31e642be0397f3660b9f18f885bd
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-05-22 21:15:39 -0400

    Suppress model loading errors for UCW

--- src/main/java/org/embeddedt/vintagefix/mixin/late/ucw/MixinModelLoaderEarlyView.java
+++ src/main/java/org/embeddedt/vintagefix/mixin/late/ucw/MixinModelLoaderEarlyView.java
@@ -1,21 +1,37 @@
 package org.embeddedt.vintagefix.mixin.late.ucw;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.IModel;
+import org.embeddedt.vintagefix.VintageFix;
 import org.embeddedt.vintagefix.dynamicresources.model.DynamicModelProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import pl.asie.ucw.util.ModelLoaderEarlyView;
 
 import java.util.Map;
 
 @Mixin(ModelLoaderEarlyView.class)
 public class MixinModelLoaderEarlyView {
+    private IModel missing;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void storeMissing(CallbackInfo ci) {
+        missing = DynamicModelProvider.instance.getObject(new ModelResourceLocation("builtin/missing", "missing"));
+    }
+
     @Redirect(method = "getModel(Lnet/minecraft/client/renderer/block/model/ModelResourceLocation;)Lnet/minecraftforge/client/model/IModel;", at = @At(value = "INVOKE", target = "Ljava/util/Map;get(Ljava/lang/Object;)Ljava/lang/Object;"), remap = false)
     private Object useVintageSecretSauce(Map map, Object location) {
         // slightly more... vintage sauce
-        return DynamicModelProvider.instance.getObject((ResourceLocation)location);
+        try {
+            return DynamicModelProvider.instance.getObject((ResourceLocation)location);
+        } catch(RuntimeException e) {
+            VintageFix.LOGGER.error("Error retrieving model {} for UCW: {}", location, e);
+            return missing;
+        }
     }
 
     @Redirect(method = "putModel", at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"), remap = false)
