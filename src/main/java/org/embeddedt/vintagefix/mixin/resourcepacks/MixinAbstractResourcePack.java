fce0ff12519deb3be3a6d41ddee3c35afa4c68e0
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-06-07 22:45:21 -0400

    Optimize locationToName slightly
    Related: #21

---
+++ src/main/java/org/embeddedt/vintagefix/mixin/resourcepacks/MixinAbstractResourcePack.java
@@ -0,0 +1,22 @@
+package org.embeddedt.vintagefix.mixin.resourcepacks;
+
+import net.minecraft.client.resources.AbstractResourcePack;
+import net.minecraft.util.ResourceLocation;
+import org.embeddedt.vintagefix.annotation.ClientOnlyMixin;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(AbstractResourcePack.class)
+@ClientOnlyMixin
+public class MixinAbstractResourcePack {
+    @Overwrite
+    private static String locationToName(ResourceLocation location) {
+        String namespace = location.getNamespace();
+        String path = location.getPath();
+        // uses a StringBuilder internally
+        return "assets/" +
+            namespace +
+            '/' +
+            path;
+    }
+}
