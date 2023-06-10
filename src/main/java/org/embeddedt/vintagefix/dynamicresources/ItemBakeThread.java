3e00b4ec2c526794f7d48fdd47857c2321f31fd4
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-06-10 15:36:06 -0400

    Add logic for baking item overrides too

--- src/main/java/org/embeddedt/vintagefix/dynamicresources/ItemBakeThread.java
+++ src/main/java/org/embeddedt/vintagefix/dynamicresources/ItemBakeThread.java
@@ -2,37 +2,40 @@ package org.embeddedt.vintagefix.dynamicresources;
 
 import com.google.common.base.Stopwatch;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ItemOverride;
+import net.minecraft.client.renderer.block.model.ItemOverrideList;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.client.resources.IResourceManagerReloadListener;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.ItemModelMesherForge;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.resource.IResourceType;
 import net.minecraftforge.client.resource.ISelectiveResourceReloadListener;
 import net.minecraftforge.client.resource.VanillaResourceType;
+import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.registries.IRegistryDelegate;
 import org.embeddedt.vintagefix.VintageFix;
 import org.embeddedt.vintagefix.core.MixinConfigPlugin;
 import org.embeddedt.vintagefix.dynamicresources.model.DynamicBakedModelProvider;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 
 public class ItemBakeThread extends Thread {
     private static volatile ItemBakeThread bakeThread = null;
-    private final List<ModelResourceLocation> locations;
+    private final LinkedList<ResourceLocation> locations;
     private final DynamicBakedModelProvider myProvider;
     volatile boolean stop = false;
 
     public static void restartBake() {
         stopAndJoin();
-        if(!MixinConfigPlugin.isMixinClassApplied("mixin.dynamic_resources.background_item_bake.BakeMixin"))
+        if(FMLClientHandler.instance().hasError() || !MixinConfigPlugin.isMixinClassApplied("mixin.dynamic_resources.background_item_bake.BakeMixin"))
             return;
         ItemModelMesherForge immf = (ItemModelMesherForge)net.minecraft.client.Minecraft.getMinecraft().getRenderItem().getItemModelMesher();
         Map<IRegistryDelegate<Item>, Int2ObjectMap<ModelResourceLocation>> theMap = ObfuscationReflectionHelper.getPrivateValue(ItemModelMesherForge.class, immf, "locations");
@@ -45,7 +48,7 @@ public class ItemBakeThread extends Thread {
     }
 
     ItemBakeThread(List<ModelResourceLocation> locations) {
-        this.locations = locations;
+        this.locations = new LinkedList<>(locations);
         this.myProvider = DynamicBakedModelProvider.instance;
         this.setName("VintageFix item baking thread");
         this.setPriority(Thread.MIN_PRIORITY);
@@ -54,18 +57,32 @@ public class ItemBakeThread extends Thread {
     @Override
     public void run() {
         Stopwatch stopwatch = Stopwatch.createStarted();
-        Iterator<ModelResourceLocation> iterator = locations.iterator();
         int numBaked = 0;
         int total = locations.size();
         long lastPrint = System.nanoTime();
         long printDelta = TimeUnit.MILLISECONDS.toNanos(5000);
         VintageFix.LOGGER.info("Baking {} models in background", total);
-        while(!stop && iterator.hasNext()) {
-            ModelResourceLocation location = iterator.next();
+        Set<ResourceLocation> processedLocations = new ObjectOpenHashSet<>();
+        while(!stop && !locations.isEmpty()) {
+            ResourceLocation location = locations.removeFirst();
+            processedLocations.add(location);
+            ModelResourceLocation modelKey = location instanceof ModelResourceLocation ? (ModelResourceLocation)location : ModelLoader.getInventoryVariant(location.toString());
             try {
-                IBakedModel theModel = myProvider.getObject(location);
-                if(theModel != null)
-                    myProvider.putObject(location, theModel);
+                IBakedModel theModel = myProvider.getObject(modelKey);
+                if(theModel != null) {
+                    myProvider.putObject(modelKey, theModel);
+                    /*
+                    ItemOverrideList overrides = theModel.getOverrides();
+                    if(overrides != null) {
+                        for(ItemOverride override : overrides.getOverrides()) {
+                            if(!processedLocations.contains(override.getLocation())) {
+                                locations.add(override.getLocation());
+                                total++;
+                            }
+                        }
+                    }
+                    */
+                }
             } catch(Throwable e) {
                 VintageFix.LOGGER.error("Error baking {}: {}", location, e);
             }
