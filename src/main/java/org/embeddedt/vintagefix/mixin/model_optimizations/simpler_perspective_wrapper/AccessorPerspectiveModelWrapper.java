ad6946f73d6fb6637f157206747b69239f9b0f68
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-08-03 10:12:35 -0400

    Port PerspectiveModelWrapper optimization

---
+++ src/main/java/org/embeddedt/vintagefix/mixin/model_optimizations/simpler_perspective_wrapper/AccessorPerspectiveModelWrapper.java
@@ -0,0 +1,12 @@
+package org.embeddedt.vintagefix.mixin.model_optimizations.simpler_perspective_wrapper;
+
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraftforge.client.model.PerspectiveMapWrapper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(PerspectiveMapWrapper.class)
+public interface AccessorPerspectiveModelWrapper {
+    @Accessor("parent")
+    IBakedModel getParent();
+}
---
+++ src/main/java/org/embeddedt/vintagefix/mixin/model_optimizations/simpler_perspective_wrapper/MixinVanillaModelWrapper.java
@@ -0,0 +1,43 @@
+package org.embeddedt.vintagefix.mixin.model_optimizations.simpler_perspective_wrapper;
+
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ModelBlock;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.vertex.VertexFormat;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.PerspectiveMapWrapper;
+import net.minecraftforge.client.model.animation.ModelBlockAnimation;
+import net.minecraftforge.common.model.IModelState;
+import net.minecraftforge.common.model.TRSRTransformation;
+import org.embeddedt.vintagefix.annotation.ClientOnlyMixin;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+import java.util.function.Function;
+
+@Mixin(targets = "net/minecraftforge/client/model/ModelLoader$VanillaModelWrapper")
+@ClientOnlyMixin
+public class MixinVanillaModelWrapper {
+    @Shadow
+    @Final
+    private ModelBlockAnimation animation;
+
+    /**
+     * Avoid storing a PerspectiveModelWrapper anonymous class with special handling for animations if
+     * there is no model animation. Ported from FoamFix.
+     */
+    @Inject(method = "bakeNormal", at = @At("RETURN"), remap = false, cancellable = true)
+    private void useRegularPerspectiveWrapper(ModelBlock model, IModelState perState, final IModelState modelState, List<TRSRTransformation> newTransforms, final VertexFormat format, final Function<ResourceLocation, TextureAtlasSprite> bakedTextureGetter, boolean uvLocked, CallbackInfoReturnable<IBakedModel> cir) {
+        if(cir.getReturnValue() instanceof PerspectiveMapWrapper
+            && cir.getReturnValue().getClass() != PerspectiveMapWrapper.class
+            && (animation == null || animation.getClips().isEmpty())) {
+            // reconstruct a simpler, smaller wrapper object
+            cir.setReturnValue(new PerspectiveMapWrapper(((AccessorPerspectiveModelWrapper)cir.getReturnValue()).getParent(), perState));
+        }
+    }
+}
