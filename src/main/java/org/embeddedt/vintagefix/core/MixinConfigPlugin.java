a4dcedbd3bc391b301dc57da2dc3db8efc6f473a
Martijn van den Brand <MartijnvandenBrand@outlook.com>
2023-08-19 23:33:54 +0200

    Remove check for Fusion in MixinConfigPlugin.java

--- src/main/java/org/embeddedt/vintagefix/core/MixinConfigPlugin.java
+++ src/main/java/org/embeddedt/vintagefix/core/MixinConfigPlugin.java
@@ -170,7 +170,7 @@ public class MixinConfigPlugin implements IMixinConfigPlugin {
     public static boolean isMixinClassApplied(String name) {
         String baseName = mixinClassNameToBaseName(name);
         // texture optimization causes issues when OF is installed
-        if(baseName.startsWith("mixin.textures") && (VintageFixCore.OPTIFINE || VintageFixCore.FUSION)) {
+        if(baseName.startsWith("mixin.textures") && VintageFixCore.OPTIFINE) {
             return false;
         }
         boolean isEnabled = Boolean.parseBoolean(config.getProperty(baseName, ""));
