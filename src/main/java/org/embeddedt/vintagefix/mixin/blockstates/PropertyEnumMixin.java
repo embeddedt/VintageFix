759256932a26058ec4178ea05fc45bc8011e2111
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-09-17 11:36:13 -0400

    Add back property optimizations with adjusted priority
    Related: #77

---
+++ src/main/java/org/embeddedt/vintagefix/mixin/blockstates/PropertyEnumMixin.java
@@ -0,0 +1,36 @@
+package org.embeddedt.vintagefix.mixin.blockstates;
+
+import com.google.common.collect.ImmutableSet;
+import net.minecraft.block.properties.PropertyEnum;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Map;
+
+@Mixin(value = PropertyEnum.class, priority = 800)
+public class PropertyEnumMixin<T> {
+    @Shadow
+    @Final
+    private ImmutableSet<T> allowedValues;
+    @Shadow
+    @Final
+    private Map<String, T> nameToValue;
+    private int vfix_hashCode;
+
+    /**
+     * @author embeddedt
+     * @reason cache
+     */
+    @Overwrite
+    public int hashCode() {
+        if(vfix_hashCode == 0) {
+            int i = super.hashCode();
+            i = 31 * i + this.allowedValues.hashCode();
+            i = 31 * i + this.nameToValue.hashCode();
+            vfix_hashCode = i;
+        }
+        return vfix_hashCode;
+    }
+}
