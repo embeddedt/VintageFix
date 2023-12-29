14d18dda68b5c3ae632c0ceac488624d4e5bfe93
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-12-29 16:53:11 -0500

    More speed for PropertyEnum

--- src/main/java/org/embeddedt/vintagefix/mixin/blockstates/PropertyEnumMixin.java
+++ src/main/java/org/embeddedt/vintagefix/mixin/blockstates/PropertyEnumMixin.java
@@ -2,15 +2,21 @@ package org.embeddedt.vintagefix.mixin.blockstates;
 
 import com.google.common.collect.ImmutableSet;
 import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.properties.PropertyHelper;
+import net.minecraft.util.IStringSerializable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
 
 @Mixin(value = PropertyEnum.class, priority = 800)
-public class PropertyEnumMixin<T> {
+public abstract class PropertyEnumMixin<T extends Enum<T> & IStringSerializable> extends PropertyHelper<T> {
     @Shadow
     @Final
     private ImmutableSet<T> allowedValues;
@@ -19,18 +25,33 @@ public class PropertyEnumMixin<T> {
     private Map<String, T> nameToValue;
     private int vfix_hashCode;
 
+    protected PropertyEnumMixin(String name, Class<T> valueClass) {
+        super(name, valueClass);
+    }
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void computeHash(CallbackInfo ci) {
+        int i = super.hashCode();
+        i = 31 * i + this.allowedValues.hashCode();
+        i = 31 * i + this.nameToValue.hashCode();
+        vfix_hashCode = i;
+    }
+
+    /**
+     * @author asiekierka
+     * @reason If the value classes are equal, we can skip checking all the entries in the maps.
+     */
+    @Redirect(method = "equals", at = @At(value = "INVOKE", target = "Ljava/util/Map;equals(Ljava/lang/Object;)Z", ordinal = 0))
+    private boolean areNameMapsEqual(Map map, Object o, Object p_equals_1_) {
+        return ((PropertyEnum<?>)p_equals_1_).getValueClass() == this.getValueClass() || map.equals(o);
+    }
+
     /**
      * @author embeddedt
      * @reason cache
      */
     @Overwrite
     public int hashCode() {
-        if(vfix_hashCode == 0) {
-            int i = super.hashCode();
-            i = 31 * i + this.allowedValues.hashCode();
-            i = 31 * i + this.nameToValue.hashCode();
-            vfix_hashCode = i;
-        }
         return vfix_hashCode;
     }
 }
