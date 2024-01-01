52ba0bf4b25e2b698d30ba4e0839296221584590
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-01-01 17:23:42 -0500

    Use fastutil map during state construction

--- src/main/java/org/embeddedt/vintagefix/mixin/blockstates/BlockStateContainerMixin.java
+++ src/main/java/org/embeddedt/vintagefix/mixin/blockstates/BlockStateContainerMixin.java
@@ -1,6 +1,7 @@
 package org.embeddedt.vintagefix.mixin.blockstates;
 
 import com.google.common.collect.ImmutableSortedMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.BlockStateContainer;
 import org.embeddedt.vintagefix.fastmap.FakeStateMap;
@@ -17,6 +18,11 @@ public class BlockStateContainerMixin {
     @Shadow
     @Final
     private ImmutableSortedMap<String, IProperty<?>> properties;
+
+    @ModifyVariable(method = "<init>(Lnet/minecraft/block/Block;[Lnet/minecraft/block/properties/IProperty;Lcom/google/common/collect/ImmutableMap;)V", at = @At(value = "INVOKE_ASSIGN", target = "Lcom/google/common/collect/Maps;newHashMap()Ljava/util/HashMap;"), ordinal = 0, index = 4)
+    private Map<String, IProperty<?>> useFastUtilMap(Map<String, IProperty<?>> map) {
+        return new Object2ObjectOpenHashMap<>(map);
+    }
 
     @ModifyVariable(method = "<init>(Lnet/minecraft/block/Block;[Lnet/minecraft/block/properties/IProperty;Lcom/google/common/collect/ImmutableMap;)V", at = @At(value = "INVOKE_ASSIGN", target = "Lcom/google/common/collect/Maps;newLinkedHashMap()Ljava/util/LinkedHashMap;"), ordinal = 1, index = 5)
     private Map<Map<IProperty<?>, Comparable<?>>, BlockStateContainer.StateImplementation> useArrayMap(Map<Map<IProperty<?>, Comparable<?>>, BlockStateContainer.StateImplementation> in) {
