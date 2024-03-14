a24848d91d63e385eb5fb8cf2218dfe0824c7748
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-03-14 10:08:29 -0400

    Make accessor mutable

--- src/main/java/org/embeddedt/vintagefix/mixin/allocation_rate/AccessorBiomeColorEvent.java
+++ src/main/java/org/embeddedt/vintagefix/mixin/allocation_rate/AccessorBiomeColorEvent.java
@@ -3,11 +3,13 @@ package org.embeddedt.vintagefix.mixin.allocation_rate;
 import net.minecraftforge.event.terraingen.BiomeEvent;
 import org.embeddedt.vintagefix.annotation.ClientOnlyMixin;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(BiomeEvent.BiomeColor.class)
 @ClientOnlyMixin
 public interface AccessorBiomeColorEvent {
     @Accessor(value = "originalColor", remap = false)
+    @Mutable
     void setOriginalColor(int value);
 }
