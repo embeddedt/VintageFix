cd2ed9327be919150954f1ec58956a3590d04135
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-03-30 21:57:47 -0400

    Fix chunk_access mixin not working with Cubic Chunks

--- src/main/java/org/embeddedt/vintagefix/mixin/chunk_access/ChunkMixin.java
+++ src/main/java/org/embeddedt/vintagefix/mixin/chunk_access/ChunkMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(Chunk.class)
+@Mixin(value = Chunk.class, priority = 500)
 public class ChunkMixin {
     @Shadow
     @Final
