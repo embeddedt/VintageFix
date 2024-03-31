a7c0e1733ed879ce4b23752ea4a339477ad61a7b
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-03-30 20:51:25 -0400

    Mark LightUtil mixin as client-only

--- src/main/java/org/embeddedt/vintagefix/mixin/dynamic_resources/MixinLightUtil.java
+++ src/main/java/org/embeddedt/vintagefix/mixin/dynamic_resources/MixinLightUtil.java
@@ -3,6 +3,7 @@ package org.embeddedt.vintagefix.mixin.dynamic_resources;
 import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.client.renderer.vertex.VertexFormatElement;
 import net.minecraftforge.client.model.pipeline.LightUtil;
+import org.embeddedt.vintagefix.annotation.ClientOnlyMixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LightUtil.class)
+@ClientOnlyMixin
 public class MixinLightUtil {
     @Inject(method = "unpack", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILHARD, remap = false)
     private static void unpackPositionW(int[] from, float[] to, VertexFormat formatFrom, int v, int e, CallbackInfo ci, int length, VertexFormatElement fromElement) {
