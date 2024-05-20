00cc1220703caabaec81da42ea3b3fd4f2023c6b
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-05-19 20:14:46 -0400

    Avoid pointless work in WorldClient#refreshVisibleChunks

--- src/main/java/org/embeddedt/vintagefix/mixin/allocation_rate/MixinWorldClient.java
+++ src/main/java/org/embeddedt/vintagefix/mixin/allocation_rate/MixinWorldClient.java
@@ -1,10 +1,12 @@
 package org.embeddedt.vintagefix.mixin.allocation_rate;
 
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.profiler.Profiler;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.WorldSettings;
 import org.embeddedt.vintagefix.annotation.ClientOnlyMixin;
@@ -29,6 +31,10 @@ public class MixinWorldClient {
     @Final
     private Set<ChunkPos> previousActiveChunkSet;
 
+    @Shadow
+    @Final
+    private Minecraft mc;
+
     /**
      * @author embeddedt
      * @reason Replace these sets with faster and less allocation-heavy alternatives. This is done once at world creation,
@@ -38,5 +44,26 @@ public class MixinWorldClient {
     private void replaceChunkSets(NetHandlerPlayClient netHandler, WorldSettings settings, int dimension, EnumDifficulty difficulty, Profiler profilerIn, CallbackInfo ci) {
         this.visibleChunks = new ObjectOpenHashSet<>(this.visibleChunks);
         this.previousActiveChunkSet = new ObjectOpenHashSet<>(this.previousActiveChunkSet);
+    }
+
+    private int vfix$lastSectionX = Integer.MIN_VALUE, vfix$lastSectionZ = Integer.MIN_VALUE, vfix$lastRenderDistance = 0;
+
+    /**
+     * @author embeddedt
+     * @reason only repopulate this set when player moves into a new chunk section
+     */
+    @Inject(method = "refreshVisibleChunks", at = @At("HEAD"), cancellable = true)
+    private void skipIfSectionUnchanged(CallbackInfo ci) {
+        int sx = MathHelper.floor(this.mc.player.posX / 16.0D);
+        int sz = MathHelper.floor(this.mc.player.posZ / 16.0D);
+
+        if(sx == vfix$lastSectionX && sz == vfix$lastSectionZ && this.mc.gameSettings.renderDistanceChunks == vfix$lastRenderDistance) {
+            ci.cancel();
+            return;
+        }
+
+        vfix$lastSectionX = sx;
+        vfix$lastSectionZ = sz;
+        vfix$lastRenderDistance = this.mc.gameSettings.renderDistanceChunks;
     }
 }
