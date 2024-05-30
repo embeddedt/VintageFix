62eb01aac7dd5769412a85f875f741f374a84132
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-05-29 21:39:05 -0400

    Mark mixin as client only

--- src/main/java/org/embeddedt/vintagefix/mixin/dynamic_resources/MixinBlockStateBase.java
+++ src/main/java/org/embeddedt/vintagefix/mixin/dynamic_resources/MixinBlockStateBase.java
@@ -2,10 +2,12 @@ package org.embeddedt.vintagefix.mixin.dynamic_resources;
 
 import net.minecraft.block.state.BlockStateBase;
 import net.minecraft.client.renderer.block.model.IBakedModel;
+import org.embeddedt.vintagefix.annotation.ClientOnlyMixin;
 import org.embeddedt.vintagefix.dynamicresources.IModelHoldingBlockState;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(BlockStateBase.class)
+@ClientOnlyMixin
 public class MixinBlockStateBase implements IModelHoldingBlockState {
     private volatile IBakedModel vfix$model;
 
