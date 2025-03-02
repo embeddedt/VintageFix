ba951784f07a2aa37e7775f6b0d9d510166dd7ac
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-03-02 15:44:45 -0500

    Fix mipmap generation not being thread-safe in vanilla
    Thanks to @kurrycat2004 for spotting this

---
+++ src/main/java/org/embeddedt/vintagefix/mixin/textures/MixinTextureUtil.java
@@ -0,0 +1,24 @@
+package org.embeddedt.vintagefix.mixin.textures;
+
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import net.minecraft.client.renderer.texture.TextureUtil;
+import org.embeddedt.vintagefix.annotation.ClientOnlyMixin;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(TextureUtil.class)
+@ClientOnlyMixin
+public class MixinTextureUtil {
+    @Redirect(method = "blendColors", at = @At(value = "FIELD", opcode = Opcodes.GETSTATIC, target = "Lnet/minecraft/client/renderer/texture/TextureUtil;MIPMAP_BUFFER:[I"))
+    private static int[] useLocalMipmapBuffer(@Share("mipmapBuffer") LocalRef<int[]> buffer) {
+        int[] buf = buffer.get();
+        if (buf == null) {
+            buf = new int[4];
+            buffer.set(buf);
+        }
+        return buf;
+    }
+}
